d49bb57f45c57d8c8adf1d1c54848cb447efe556
ishantrana <ishant@atcouch.com>
2015-11-28 22:51:21 +0530

    basic calculator

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,61 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsTyping = false
+    // can infer by its own
+   // var userIsTyping: Bool = false
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsTyping{
+            enter()
+        }
+        switch operation {
+        case "×": performOperation{ $0 * $1}
+        case "÷": performOperation{ $1 / $0}
+        case "+": performOperation{ $0 + $1}
+        case "−": performOperation{ $1 - $0}
+        default :break
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    func performOperation(operation: (Double,Double)->Double){
+        if operandStack.count>=2{
+            displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+        }
     }
-
-
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle! // unwrap the optional and crash if its nil
+        if userIsTyping{
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+            userIsTyping = true
+        }
+    
+//        print("digit = + \(digit)")
+    }
+    
+    var operandStack = Array<Double>()
+    @IBAction func enter() {
+        userIsTyping = false
+        operandStack.append(displayValue)
+        print("operandStack + \(operandStack)")
+    }
+    
+    var displayValue: Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+        }
+    }
 }
 
